75229a1dfa24189d600c0eee78a7f891c02a2e69
elusiivee <andrii.1669@gmail.com>
2023-10-02 20:08:42 +0200

    added pie chart and low poly brane img

--- Portfolio/src/components/MainComponent.vue
+++ Portfolio/src/components/MainComponent.vue
@@ -1,4 +1,5 @@
 <script>
+import piechart from './PieChart.vue'   
 document.addEventListener('mousemove', parallax);
 function parallax(e){
     document.querySelectorAll('.object').forEach(function(move){
@@ -14,12 +15,12 @@ function parallax(e){
 
 <template>
     <div class="my_body">
-        <section class="parallax"></section>
+        <div class="main_bg">
         <div class="main_container row">
             <div class="col-3">
                 <div class="layer1 object" data-value="0.03"><img src="./icons/IMG_20230927_130243_242.jpg" style="right: -53px; top: 50px; z-index: 1;"></div>
                 <div class="layer1 object" data-value="0.02"><img src="./icons/image_2023-09-27_19-36-44.png" ></div>
-                <div class="layer1 object" data-value="0.01"><img src="./icons/IMG_20230927_130530_740.jpg" style="right: -52px; top: -60px;"></div>
+                <div class="layer1 object" data-value="0.01"><img src="./icons/IMG_20230927_130530_740.jpg" style="right: -52px; top: -83px;"></div>
             </div>
             <div class="col-6">
                 <div class="object" data-value="0.01">
@@ -33,6 +34,16 @@ function parallax(e){
                 <div class="layer2 object" data-value="0.02"><img src="./icons/image_2023-09-27_19-37-16.png" ></div>
                 <div class="layer2 object" data-value="0.01"><img src="./icons/IMG_20230927_130808_779.jpg" style="right: 38px; top: -60px;" ></div>
             </div>
+        </div>
+        <div class="line" style="margin-top: 2rem;"></div>
+    </div>
+        <div class="container">
+            <div class="bold_text" style="text-align: center; color: #33363c; text-decoration: underline; margin-bottom: 2rem;">My skills</div>
+            <div class="row">
+                <div class="col-6"><piechart/></div>
+                <div class="col-6 poly_container"><img src="./icons/brain.png" class="brain"  alt=""></div>
+            </div>
+            
         </div>
     </div>
     
@@ -49,17 +60,21 @@ function parallax(e){
 
 .bold_text{
     font-family: Kanit;
-    font-weight: 700;  
+    font-weight: 700; 
+    font-size: 50px; 
 }
 
 .my_body {
     min-width: 900px;
   margin: 0;
   padding: 0;
-  background-color: #FFCC70;
+  background-color: #fff0d3;
   justify-content: center; 
   align-items: center;
   color: white;
+}
+.main_bg{
+    background-color: #FFCC70;
 }
 .main_container{
     position: relative;
@@ -70,7 +85,14 @@ function parallax(e){
     height: 85vh;
     margin: 0 auto;
     padding: 0 20px;
-    }
+}
+.container{
+    position: relative;
+    justify-content: center; 
+    align-items: center;
+    max-width: 100%; 
+    width: 80%;
+}
 .layer1{
     top:0;
     left: 0;
@@ -86,8 +108,8 @@ function parallax(e){
 
 .main_container img{
     position: relative;
-    width: 200px;
-    height: 255px;
+    width: 60%;
+    height: 50%;
     object-fit: cover;
     border-radius: 1rem;
 }
@@ -107,16 +129,34 @@ function parallax(e){
     letter-spacing: 10px;
 
 }
-/* .layer:nth-child(1){
-    background-image: url(icons/IMG_20230927_130516_271.jpg);
+.line{
+    height: 2px;
+  width: 100%; 
+  background-color:  #ed902e;
 }
-.layer:nth-child(2){
-    background-image: url(icons/IMG_20230927_130530_740.jpg);
+.poly_container{
+    display: flex;
+    justify-content: center; 
+  align-items: center;
 }
-.layer:nth-child(3){
-    background-image: url(icons/IMG_20230927_130541_952.jpg);
+.brain{
+    width: 70%;
+    animation: moon-move 10s infinite ease-in-out;
 }
-.layer:nth-child(4){
-    background-image: url(icons/IMG_20230927_130808_779.jpg);
-} */
+
+@keyframes moon-move {
+    from{
+        transform: translateX(0%) translateY(-5%);
+        
+    }
+
+    50%{
+        transform: translateX(0%) translateY(-15%);
+    }
+
+    to{
+        transform: translateX(0%) translateY(0%);
+    }
+}
+
 </style>
--- Portfolio/src/main.js
+++ Portfolio/src/main.js
@@ -2,10 +2,12 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
+import PieChart from './components/PieChart.vue'; // Adjust the path as needed
 
 
 createApp(App)
   .use(router)
+  .component('piechart', PieChart)
   .mount("#app");
 
 import "bootstrap/dist/js/bootstrap.js";
